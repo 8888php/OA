f2270ef1bcbb7d9b1e6eb8257d943111db131e34
rain <rain>
2017-07-30 16:50:26 +0800

    果树所借款单

--- app/Controller/RequestNoteController.php
+++ app/Controller/RequestNoteController.php
@@ -6,7 +6,7 @@ App::uses('AppController', 'Controller');
 class RequestNoteController extends AppController {
 
     public $name = 'RequestNote';
-    public $uses = array('ResearchProject', 'User', 'ResearchCost', 'ResearchSource','ProjectMember', 'ApplyMain', 'ApplyBaoxiaohuizong', 'ApprovalInformation','Department', 'ApplyPaidleave', 'ChailvfeiSqd');
+    public $uses = array('ResearchProject', 'User', 'ResearchCost', 'ResearchSource','ProjectMember', 'ApplyMain', 'ApplyBaoxiaohuizong', 'ApprovalInformation','Department', 'ApplyPaidleave', 'ChailvfeiSqd', 'ApplyJiekuandan');
 
     public $layout = 'blank';
     public $components = array('Cookie', 'Approval');
@@ -157,7 +157,7 @@ class RequestNoteController extends AppController {
      public function gss_loan() {
          
         if ($this->request->is('ajax')) {
-            $this->sub_declares($_POST);
+            $this->gss_loan_save($_POST);
         }else{
         
         $this->render();
@@ -316,7 +316,7 @@ class RequestNoteController extends AppController {
             $this->ret_arr['msg'] = '参数有误';
             exit(json_encode($this->ret_arr));
         }
-        $table_name = 't_chailvfei_sqd';
+        $table_name = 'chailvfei_sqd';
         $p_id = 4;//审批流id
         $project_id = 0;
         $type = 0;//类型暂定为0
@@ -352,7 +352,7 @@ class RequestNoteController extends AppController {
         $attrArr['create_time'] = date('Y-m-d H:i:s', time());
         
         # 开始入库
-        $this->ApplyPaidleave->begin();
+        $this->ChailvfeiSqd->begin();
         $attrId = $this->ChailvfeiSqd->add($attrArr);
         
         # 主表入库
@@ -380,6 +380,124 @@ class RequestNoteController extends AppController {
             $this->ChailvfeiSqd->rollback();
         }
         $mainId ? $commitId = $this->ChailvfeiSqd->rollback() : $commitId = $this->ChailvfeiSqd->commit();
+
+
+        if ($commitId) {
+            //如果审批通过，且跳过下个则在表里记录一下
+            if (!empty($ret_arr['code_id']) ) {
+                foreach ($ret_arr['code_id'] as $k=>$v) {
+                    if ($v == $this->userInfo->id) {
+                        $save_approve = array(
+                            'main_id' => $mainId,
+                            'position_id' => $this->userInfo->position_id,
+                            'approve_id' => $this->userInfo->id,
+                            'remarks' => '',
+                            'name' => $this->userInfo->name,
+                            'ctime' => date('Y-m-d H:i:s', time()),
+                            'status' => 1
+                        );
+                    } else {
+                        //根据id取出当前用户的信息
+                        $userinfo = $this->User->findById($v);
+                        $save_approve = array(
+                            'main_id' => $mainId,
+                            'position_id' => $userinfo['User']['position_id'],
+                            'approve_id' => $v,
+                            'remarks' => '',
+                            'name' => $userinfo['User']['name'],
+                            'ctime' => date('Y-m-d H:i:s', time()),
+                            'status' => 1
+                        );
+                    }
+                   $this->ApprovalInformation->add($save_approve);
+                }
+                
+            } else {
+                //其他审批人 暂时不处理
+            }
+            $this->ret_arr['code'] = 0;
+            $this->ret_arr['msg'] = '申请成功';
+        } else {
+            $this->ret_arr['msg'] = '申请失败';
+        }
+
+
+        echo json_encode($this->ret_arr);
+        exit;
+    }
+    
+    //果树所借款单
+    private function gss_loan_save($datas) {
+        if (empty($datas['ctime']) || empty($datas['loan_reason']) 
+                || empty($datas['big_amount']) || empty($datas['small_amount']) 
+                || empty($datas['big_approval_amount']) || empty($datas['small_approval_amount'])
+                || empty($datas['repayment_plan'])) {
+            $this->ret_arr['msg'] = '参数有误';
+            exit(json_encode($this->ret_arr));
+        }
+        $table_name = 'apply_jiekuandan';
+        $p_id = 5;//审批流id
+        $project_id = 0;
+        $type = 0;//类型暂定为0
+        $ret_arr = $this->Approval->apply_create($p_id, $this->userInfo, $project_id);
+        
+//        $ret_arr = $this->get_create_approval_process_by_table_name($table_name,$type, $this->userInfo->department_id);
+//
+//        if ($ret_arr[$this->code] == 1) {
+//            $this->ret_arr['msg'] = $ret_arr[$this->msg];
+//            exit(json_encode($this->ret_arr));
+//        }
+        #附表入库
+        //是部门，取当前用户的部门信息
+        $department_id = $this->userInfo->department_id;
+        $department_arr = $this->Department->findById($department_id);
+        $department_name = !empty($department_arr) ? $department_arr['Department']['name'] : '';
+        $department_fzr = !empty($department_arr) ? $department_arr['Department']['user_id'] : 0;  // 部门负责人
+        
+        $attrArr = array();
+        $attrArr['ctime'] = $datas['ctime'];
+        $attrArr['reason'] = $datas['loan_reason'];
+        $attrArr['department_id'] = $department_id;
+        $attrArr['department_name'] = $department_name;
+        $attrArr['project_id'] = $project_id;
+        
+        $attrArr['apply_money_capital'] = $datas['big_amount'];
+        $attrArr['apply_money'] = $datas['small_amount'];
+        $attrArr['approve_money_capital'] = $datas['big_approval_amount'];
+        $attrArr['approve_money'] = $datas['small_approval_amount'];
+        $attrArr['repayment'] = $datas['repayment_plan'];
+        $attrArr['user_id'] = $this->userInfo->id;
+        $attrArr['create_time'] = date('Y-m-d H:i:s', time());
+        
+        # 开始入库
+        $this->ApplyJiekuandan->begin();
+        $attrId = $this->ApplyJiekuandan->add($attrArr);
+        
+        # 主表入库
+        $mainArr = array();
+        $mainArr['next_approver_id'] = $ret_arr['next_id'];//下一个审批职务的id
+        $mainArr['code'] = $ret_arr['code'];//当前单子审批的状态码
+        $mainArr['approval_process_id'] = $p_id; //审批流程id
+        $mainArr['type'] = $type; 
+        $mainArr['attachment'] = ''; 
+        $mainArr['name'] = '果树所出差审批单';
+        $mainArr['project_id'] = $project_id;
+        $mainArr['department_id'] = $department_id;        
+        $mainArr['table_name'] = $table_name;
+        $mainArr['user_id'] = $this->userInfo->id;
+        $mainArr['total'] = 0;
+        $mainArr['attr_id'] = $attrId;
+        $mainArr['project_user_id'] = 0;
+        $mainArr['project_team_user_id'] = 0;
+        $mainArr['department_fzr'] = $department_fzr; // 行政 申请所属部门负责人
+        $mainArr['ctime'] = date('Y-m-d H:i:s', time());
+        $mainArr['subject'] = '';
+        if ($attrId) {
+            $mainId = $this->ApplyMain->add($mainArr);
+        } else {
+            $this->ApplyJiekuandan->rollback();
+        }
+        $mainId ? $commitId = $this->ApplyJiekuandan->rollback() : $commitId = $this->ApplyJiekuandan->commit();
 
 
         if ($commitId) {
